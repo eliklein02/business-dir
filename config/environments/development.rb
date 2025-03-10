1da1d9369ab927ba3a2a482db08c1958dac3b3e7
Eli Klein <eliklein02@gmail.com>
2025-03-10 11:37:31 -0400

    hosts

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,6 +17,10 @@ Rails.application.configure do
   # Enable server timing.
   config.server_timing = true
 
+
+  config.hosts << "192dnsserver.com"
+
+
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join("tmp/caching-dev.txt").exist?
