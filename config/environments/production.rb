b9da28d94cfab15ff9f59dd10c0684db77da657f
Eli Klein <eliklein02@gmail.com>
2025-03-10 11:36:13 -0400

    host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,6 +51,8 @@ Rails.application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
 
+  confic.hosts << "192dnsserver.com"
+
   # Skip http-to-https redirect for the default health check endpoint.
   # config.ssl_options = { redirect: { exclude: ->(request) { request.path == "/up" } } }
 
