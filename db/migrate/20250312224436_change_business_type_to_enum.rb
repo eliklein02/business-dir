86f15c9f4419958edd682c650f5f4dcca69e6b71
Eli Klein <eliklein02@gmail.com>
2025-03-17 23:42:29 -0400

    added type

--- db/migrate/20250312224436_change_business_type_to_enum.rb
+++ db/migrate/20250312224436_change_business_type_to_enum.rb
@@ -1,6 +1,6 @@
 class ChangeBusinessTypeToEnum < ActiveRecord::Migration[7.2]
   def change
-    remove_column :businesses, :business_type
+    remove_column :businesses, :business_type, :string
     add_column :businesses, :business_type, :integer
   end
 end
